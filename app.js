3c553e19f92e3c3d9307b1cc14ff934a979965f6
Seojin Kim <ksj855210@naver.com>
2022-05-06 04:38:29 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var style1Router = require('./routes/style1');
+var question1Router = require('./routes/question1');
+var question2Router = require('./routes/question2');
+var question3Router = require('./routes/question3');
+var question4Router = require('./routes/question4');
+var question5Router = require('./routes/question5');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/style1', style1Router);
+app.use('/question1', question1Router);
+app.use('/question2', question2Router);
+app.use('/question3', question3Router);
+app.use('/question4', question4Router);
+app.use('/question5', question5Router);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
